306153938c3e98dd594ffa5ab8f0bea2eac2c406
Yovana <yoyosworldtv@yahoo.com>
2025-06-27 19:10:50 -0700

    created a function that saves text input and displays as list using the add button

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-document.open("Testing, Hello World");
+
+
+// this gets the text input value 
+function createListFromInput() {
+
+    const textInput = document.getElementById("todoInput");
+    const inputValue= textInput.value;
+    
+    
+    const listContainer = document.getElementById("todoList");
+    
+  if (inputValue !== ""){
+    const listItem = document.createElement('li');
+    listItem.textContent = inputValue;
+
+    listContainer.appendChild(listItem);
+
+    textInput.value = "";
+}
+}
+
